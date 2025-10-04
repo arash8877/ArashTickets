f4213dced2dab582f7dd4a3d866201dd946b37ea
Arash <arash8877@gmail.com>
2025-10-04 11:31:12 +0200

    build Ticket entity

---
+++ backend/core/entities/Ticket.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
+
+namespace backend.core.entities
+{
+    public class Ticket
+    {
+        [Key]
+        public long Id { get; set; }
+        public DateTime Time { get; set; }
+
+        public string PassengersName { get; set; }
+        public long PassengerSSN { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public int Price { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public string ConfidentialComment { get; set; } = "Normal";
+    }
+}
--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f6462f2a519b229358bd0fe9201e073796e2448")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e22e3d11cf2bf5a74d468d480b12242bfa7fed68")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
