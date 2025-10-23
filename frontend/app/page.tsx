caf1a71f75328e77e034f79554c2c0a3fe43a73c
Arash <arash8877@gmail.com>
2025-10-23 17:26:40 +0200

    create the home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,18 @@
-import CustomTitle from "@/components/CustomTitle";
+import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="container mx-auto px-4">
-      <CustomTitle title="Welcome to ArashTicket!" />
+    <div className="page-container">
+      <div className="mt-16">
+        <h1 className="relative text-xl sm:text-3xl font-bold">
+          Welcome to ArashTicket
+          <div className="absolute inset-0 w-full h-8 sm:h-12 rounded-full bg-linear-to-t from-purple-500 to-blue-300 blur-md opacity-40"></div>
+        </h1>
+      </div>
+      <Link href="/tickets" className="bg-sky-800 text-white px-4 py-2 rounded-lg">
+        Tickets
+      </Link>
     </div>
   );
 };
