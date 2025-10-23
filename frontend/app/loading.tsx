9b13de3c22e45d595c6da0672bb33331afa110f0
Arash <arash8877@gmail.com>
2025-10-23 17:43:57 +0200

    create not found page

---
+++ frontend/app/loading.tsx
@@ -0,0 +1,11 @@
+import Spinner from "@/components/Spinner";
+
+const LoadingPage = () => {
+  return (
+    <div className="page-container ">
+      <Spinner />
+    </div>
+  );
+};
+
+export default LoadingPage;
