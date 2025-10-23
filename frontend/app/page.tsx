31a45b431bb1d21b725fadb367ffd931cd5bfc64
Arash <arash8877@gmail.com>
2025-10-23 16:57:13 +0200

    create rusable breadcrumb component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,10 @@
-import BackButton from '@/components/BackButton'
+
 import React from 'react'
 
 const HomePage = () => {
   return (
     <div className='text-red-700 text-3xl font-bold'>
-      <BackButton />
+
       HomePage
       
       </div>
